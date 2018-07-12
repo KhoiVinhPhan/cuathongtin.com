b3403e164677d18f975e9d65fcf11aa6a4684042
khoivinhphan <khoivinhphan@gmail.com>
2018-07-12 22:48:28 +0700

    code_file

--- 1.SourceCode/resources/views/backend/users/index.blade.php
+++ 1.SourceCode/resources/views/backend/users/index.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.backend.app')
 
 @section('content')
+<style type="text/css" media="screen">
+	#uploadImage {
+	 	width: 0px;
+	 	height: 0px;
+	 	overflow: hidden;
+	}
+</style>
 <div>
 	<div class="col-sm-2">
-		<img id="img_avatar_face" src="http://i.9mobi.vn/cf/images/2015/03/nkk/nhung-hinh-anh-dep-11.jpg" style="width: 100%; height: auto;" class="thumbnail">
+		<img id="img_avatar_face" src="{{ asset('image_user/no_image.png') }}" style="width: 100%; height: auto;" class="thumbnail">
+		<input name="uploadImage" type="file" id="uploadImage" />
 	</div>
 	<div class="col-sm-5">
 		<div class="form-group">
@@ -21,9 +29,25 @@
 </div>
 <script>
 	$(document).ready(function(){
-		 // avatar
+		// Change avatar
         $("#img_avatar_face").click(function(){
+            $('#uploadImage').trigger('click'); 
             
+        });
+
+        $("#uploadImage").change(function(){
+        	var input = this;
+		    var url = $(this).val();
+		    var ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
+		    if (input.files && input.files[0] && (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")){
+		        var reader = new FileReader();
+		        reader.onload = function (e) {
+		           	$('#img_avatar_face').attr('src', e.target.result);
+		        }
+		       reader.readAsDataURL(input.files[0]);
+		    }else{
+		      	$('#img_avatar_face').attr('src', '/image_user/no_image.png');
+		    }
         });
 	});	
 </script>
